15142d8d8221469072d1f56a195242e761b77a27
rafael <rafael.henrique@techlead.com.br>
2023-01-20 09:08:53 -0300

    Tests coverage.

---
+++ src/test/java/com/creditproject/creditserviceapi/utils/JsonUtilsTest.java
@@ -0,0 +1,39 @@
+package com.creditproject.creditserviceapi.utils;
+
+import com.creditproject.creditserviceapi.helpers.UserAuthMockBuilder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+class JsonUtilsTest {
+    ObjectMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        mapper = spy(new ObjectMapper());
+    }
+
+    @Test
+    void shouldReturnAJSon() {
+        final var result = JsonUtils.convertToString(mapper, UserAuthMockBuilder.builder().defaultValues().defaultValues());
+        assertNotNull(result);
+    }
+
+    @Test
+    void shouldReturnNullWhenStringIsEmpty() {
+        final var result = JsonUtils.convertToString(mapper, "");
+        assertNull(result);
+    }
+
+    @Test
+    void shouldReturnNullWhenGetsJsonProcessingException() throws JsonProcessingException {
+        when(mapper.writeValueAsString(any())).thenThrow(JsonProcessingException.class);
+        assertNull(JsonUtils.convertToString(mapper, "dfgdg"));
+    }
+}
