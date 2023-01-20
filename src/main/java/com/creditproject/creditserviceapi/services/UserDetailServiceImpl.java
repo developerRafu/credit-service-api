560516946428d8210f3c3e3d771b2f37645a4ca0
rafael <rafael.henrique@techlead.com.br>
2023-01-20 09:21:41 -0300

    Multi profiles e lazy loading.

--- src/main/java/com/creditproject/creditserviceapi/services/UserDetailServiceImpl.java
+++ src/main/java/com/creditproject/creditserviceapi/services/UserDetailServiceImpl.java
@@ -4,13 +4,14 @@ import com.creditproject.creditserviceapi.domain.UserDetailsImpl;
 import com.creditproject.creditserviceapi.exceptions.UserNotFound;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RequiredArgsConstructor(onConstructor = @__({@Autowired, @Lazy}))
 public class UserDetailServiceImpl implements UserDetailsService {
   private final IUserService userService;
 
